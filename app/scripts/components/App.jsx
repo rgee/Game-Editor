3d2da2a6c1e17895c8e0b8e484a767932dc8ae71
Ryan Gee <ryan.cv.gee@gmail.com>
2016-06-18 19:32:01 -0400

    Change the theme to be more green.

--- app/scripts/components/App.jsx
+++ app/scripts/components/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { green900, green700, yellow200 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const theme = getMuiTheme({
+  palette: {
+    primary1Color: green700,
+    primary2Color: green900,
+    accent1Color: yellow200
+  }
+})
 
 class App extends React.Component {
   render() {
     return (
-      <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <MuiThemeProvider muiTheme={theme}>
         {this.props.children}
       </MuiThemeProvider>
     );
