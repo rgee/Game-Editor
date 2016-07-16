a77fb5bde82dc9c0a49b85884ddac08615b0f241
Ryan Gee <ryan.cv.gee@gmail.com>
2016-07-16 15:15:42 -0400

    Add a map you can pan around on.

--- app/scripts/components/Map/presenter.jsx
+++ app/scripts/components/Map/presenter.jsx
@@ -3,29 +3,98 @@ import React, { PropTypes } from 'react'
 
 const width = 800;
 const height = 600;
+const speed = 2;
+
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      xViewOffset: 0,
+      yViewOffset: 0
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.backgroundImageUrl !== this.props.backgroundImageUrl) {
+      this.loadBackgroundImage(nextProps.backgroundImageUrl);
+    }
+  }
+
+  loadBackgroundImage(backgroundImageUrl) {
+    const img = new Image();
+    img.onload = (() => {
+      this.setState({
+        backgroundImage: img
+      });
+    }).bind(this);
+    img.src = backgroundImageUrl;
+  }
+
   componentDidMount() {
     this.updateCanvas();
+    this.setupEvents();
+    this.loadBackgroundImage(this.props.backgroundImageUrl);
   }
 
   componentDidUpdate() {
     this.updateCanvas();
   }
 
+  setupEvents() {
+    const { canvas } = this.refs;
+    let isDown = false;
+    let initialPageX = null;
+    let initialPageY = null;
+    canvas.addEventListener('mousedown', (e) => {
+      isDown = true;
+      initialPageX = e.pageX;
+      initialPageY = e.pageY;
+    });
+
+    canvas.addEventListener('mouseup', () => {
+      isDown = false;
+    });
+
+    canvas.addEventListener('mousemove', ((e) => {
+      if (!isDown) {
+        return;
+      }
+
+      let xOffset = this.state.xViewOffset;
+      let yOffset = this.state.yViewOffset;
+
+      xOffset -= (initialPageX - e.pageX) * speed;
+      yOffset -= (initialPageY - e.pageY) * speed;
+      initialPageY = yOffset;
+      initialPageX = xOffset;
+
+      this.setState({
+        xViewOffset: xOffset,
+        yViewOffset: yOffset
+      });
+    }).bind(this));
+  }
+
   updateCanvas() {
     const {
-      backgroundImageUrl
+      backgroundImageUrl,
     } = this.props;
 
-    const ctx = this.refs.canvas.getContext('2d');
+    const {
+      yViewOffset,
+      xViewOffset,
+      backgroundImage
+    } = this.state;
+
+    const { canvas } = this.refs;
+    const ctx = canvas.getContext('2d');
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.clearRect(0, 0, width, height);
+    ctx.translate(xViewOffset, yViewOffset);
 
-    const img = new Image();
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0);
-    };
-
-    img.src = backgroundImageUrl;
+    if (backgroundImage) {
+      ctx.drawImage(backgroundImage, 0, 0);
+    }
   }
 
   render() {
@@ -38,7 +107,9 @@ class Map extends React.Component {
 }
 
 Map.PropTypes = {
-  backgroundImageUrl: PropTypes.string
-}
+  backgroundImageUrl: PropTypes.string,
+  xViewOffset: PropTypes.number,
+  yViewOffset: PropTypes.number
+};
 
 export default Map;
